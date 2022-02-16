565094f02eb72e6a99e03988b5aa11f05ea1b975
faisalknoldus <mohd.faisal@knoldus.com>
2022-02-16 13:48:05 +0530

    Initial commit

---
+++ src/main/java/FindProduction.java
@@ -0,0 +1,26 @@
+
+
+public class FindProduction {
+    public static long getResult(long num1,long num2){
+
+        //        Writing lambda for the given requirement
+        ProductionOfNumbers productionOfNumbers= ((x, y) -> {
+            long result = 1;
+            for(long i = y; i>=x;i--){
+                result = result * i;
+            }
+            return result;
+        });
+       return productionOfNumbers.getProduction(num1,num2);
+    }
+    public static void main(String[] args) {
+
+//      getting output
+        long output1 = FindProduction.getResult(5,15);
+        long output2 = FindProduction.getResult(2,2);
+
+        System.out.println(output1);
+        System.out.println(output2);
+
+    }
+}
---
+++ src/main/java/ProductionOfNumbers.java
@@ -0,0 +1,6 @@
+
+
+@FunctionalInterface
+public interface ProductionOfNumbers {
+    long getProduction(long x,long y);
+}
